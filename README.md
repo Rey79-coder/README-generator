b527579e0d3bf458c9b19d75fd6dceb4bac38598
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-06-18 13:33:12 -0700

    Progress

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# README-generator
+
+# Echeriri Covid/Killers
+
+# Table of Content
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#Licenses)
+- [Contribution](#Contribution)
+- [Link](#Link)
+- [Questions](#Questions)
+- [email](#email)
+
+## Description:
+    This video game is to promote Echeriri. Because Echeriri believes on branding is investing in different marketing fields. Potential audience are gamers, developers, and humans in general. Therefore, this free web browser video game is created. 
+## Installation:
+    You don't need any installation. Go to echeiriri.com/videogame/ to play it and have fun saving Earth.
+## Usage:
+    You can play it in any electronic device. For further instructions of how to play on specific platform visit: echeriri.com/videogame/instructions/
+## Contributing:
+    Any developer interested in being part of Echeriri Web Development Department please contact me for more details.
+## Link:
+    https://rey79-coder.github.io/README-generator/
+## Questions:
+    How are going to make profit with this video game?
+## email:
+    verareyna79@gmail.com
+
+    For more information about the License, click on the link below.
+    
+- [License](https://opensource.org/licenses/undefined)
+## Questions:
+    For questions about the Generator you can go to my 
+    GitHub page at the following Link: 
+- [GitHub Profile](https://github.com/undefined)
+For additional questions please reach out to my email at: verareyna79@gmail.com.
+    Here is a video on how to work the application.
+- [Video](https://youtu.be/HQAggkj8EWs)
--- index.js
+++ index.js
@@ -1,13 +1,221 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const { title } = require('process');
+const util = require('util');
 
-// TODO: Create a function to initialize app
-function init() {}
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// Function call to initialize app
+// function that crats the array of questions for user
+function promptUser() {
+    return inquirer.prompt(
+    [
+        {
+
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Requires title to continue' }}
+        },
+
+        // {
+        //     type: 'checkbox',
+        //     name: 'Content Table',
+        //     message: 'Enter the table of content',
+        //     choices: [
+        //         "Description",
+        //         "Installation",
+        //         "Usage",
+        //         "License",
+        //         "Contribution",
+        //         "Questions"
+        //     ],
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            // validate: (value) => { if (value) { return true } else { return 'Enter a list of the content for this Readme' }}
+        // },
+
+         // TABLE OF CONTENTS CHOICES.
+        // 1.- DESCRIPTION
+        // 2.- INSTALLATION
+        // 3.- USAGE INFO
+        // 4.- LICENSE
+        // 5.- CONTRIBUTIONS
+        // 6.- TESTS
+        // 7.- QUESTIONS
+
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Enter a brief description of the project',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Provide description to continue' }}
+        },   
+        
+      
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install your app?',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Steps needed' }}
+        },    
+        
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use your app?',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Steps needed' }}
+        },  
+
+        {
+            type: 'checkbox',
+            name: 'License',
+            message: 'Choose your license',
+            choices: [
+                "Apache",
+                "MCT",
+                "ISC",
+                "GNU GPLv3"
+            ],
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            // validate: (value) => { if (value) { return true } else { return 'Enter a list of the content for this Readme' }}
+        },
+        
+        {
+            type: 'input',
+            name: 'Contribution',
+            message: 'How to contribute to the project?',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Steps needed' }}
+        }, 
+
+        {
+            type: 'input',
+            name: 'Link',
+            message: 'Enter a deployed link?',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Enter a deployable link' }}
+        }, 
+
+        {
+            type: 'input',
+            name: 'Questions',
+            message: 'Enter questions for future development',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Enter questions for future development' }}
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            // USE INQUIRER TO VALIDATE ANSWERS.
+            validate: (value) => { if (value) { return true } else { return 'Enter questions for future development' }}
+        },
+    ]);
+}
+
+function generateMarkdown(response) {
+    return `
+# ${response.title}
+
+# Table of Content
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#Licenses)
+- [Contribution](#Contribution)
+- [Link](#Link)
+- [Questions](#Questions)
+- [email](#email)
+
+## Description:
+    ${response.Description}
+## Installation:
+    ${response.installation}
+## Usage:
+    ${response.usage}
+## Contributing:
+    ${response.Contribution}
+## Link:
+    ${response.Link}
+## Questions:
+    ${response.Questions}
+## email:
+    ${response.email}
+
+    For more information about the License, click on the link below.
+    
+- [License](https://opensource.org/licenses/${response.license})
+## Questions:
+    For questions about the Generator you can go to my 
+    GitHub page at the following Link: 
+- [GitHub Profile](https://github.com/${response.username})
+For additional questions please reach out to my email at: ${response.email}.
+    Here is a video on how to work the application.
+- [Video](https://youtu.be/HQAggkj8EWs)
+`;
+}
+
+// function to initialize program
+async function init() {
+    try {
+        const response = await promptUser();
+
+        const readMe = generateMarkdown(response);
+
+        await writeFileAsync("README.md", readMe);
+        console.log("Success!");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// function call to initialize program
 init();
+
+
+
+// ).then(({
+//     title,
+//     description,
+//     installation,
+//     link
+
+// })=> {
+
+      // TEMPLATE TO USE
+//    const template = `# ${title}
+
+//    * [title] (#title)
+//    * [description] (#description)
+//    * [installation] (#installation)
+//    * [link] (#link)
+   
+//    # title
+//    ${title}
+
+//    ## description
+//    ${description}
+
+//    ### installation
+//    ${installation}
+
+//    #### link
+//    ${link}
+
+//    // FUNCTION TO CREATE README
+//    createNew
+
+    
+// }
+// )
+
+
